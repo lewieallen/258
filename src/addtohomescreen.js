0112ccbc8df9b92ee4837791b0a1e57a5fb1af59
Lewie Allen <lewieallen@live.co.uk>
2015-06-19 16:34:24 +0100

    Amend msg

--- src/addtohomescreen.js
+++ src/addtohomescreen.js
@@ -49,7 +49,7 @@ ath.intl = {
 	},
 
 	en_us: {
-		ios: 'To add this web app to the home screen: tap %icon and then <strong>Add to Home Screen</strong>.',
+		ios: 'To make this an app in your menu: tap %icon and then <strong>Add to Home Screen</strong>.',
 		android: 'To add this web app to the home screen open the browser option menu and tap on <strong>Add to homescreen</strong>. <small>The menu can be accessed by pressing the menu hardware button if your device has one, or by tapping the top right menu icon <span class="ath-action-icon">icon</span>.</small>',
 	},
 
